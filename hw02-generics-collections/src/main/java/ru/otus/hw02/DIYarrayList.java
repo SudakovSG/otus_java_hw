21ce2ea06cefcf23e159e5a7af27c8bd5843a7bb
Sergey Sudakov <serg358@mail.ru>
2019-04-28 21:53:49 +0300

    минималистичный вариант реализации ArrayList

---
+++ hw02-generics-collections/src/main/java/ru/otus/hw02/DIYarrayList.java
@@ -0,0 +1,149 @@
+package ru.otus.hw02;
+
+import java.util.*;
+
+class DIYarrayList<T> implements List<T> {
+    private Object[] elements;
+
+    public DIYarrayList() {
+        elements = new Object[0];
+    }
+
+    @Override
+    public int size() {
+        return elements.length;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<T> iterator() { throw new UnsupportedOperationException(); }
+
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(this.elements, this.size());
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] t1s) { throw new UnsupportedOperationException(); }
+
+    @Override
+    public boolean add(T t) {
+        this.elements = Arrays.copyOf(this.elements, this.size()+1);
+        this.elements[this.size()-1] = t;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) { throw new UnsupportedOperationException(); }
+
+    @Override
+    public boolean containsAll(Collection<?> collection) { throw new UnsupportedOperationException(); }
+
+    @Override
+    public boolean addAll(Collection<? extends T> collection) { throw new UnsupportedOperationException(); }
+
+    @Override
+    public boolean addAll(int i, Collection<? extends T> collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> collection) { throw new UnsupportedOperationException(); }
+
+    @Override
+    public boolean retainAll(Collection<?> collection) { throw new UnsupportedOperationException(); }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T get(int i) {
+        if (i > this.size()-1 || i < 0) {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + this.size());
+        }
+        return (T) this.elements[i];
+    }
+
+    @Override
+    public T set(int i, T t) {
+        this.elements[i] = t;
+        return (T) this.elements[i];
+    }
+
+    @Override
+    public void add(int i, T t) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T remove(int i) { throw new UnsupportedOperationException(); }
+
+    @Override
+    public int indexOf(Object o) { throw new UnsupportedOperationException(); }
+
+    @Override
+    public int lastIndexOf(Object o) { throw new UnsupportedOperationException(); }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return new Itr();
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int i) { throw new UnsupportedOperationException(); }
+
+    @Override
+    public List<T> subList(int i, int i1) { throw new UnsupportedOperationException(); }
+
+    private class Itr implements ListIterator<T> {
+        private int cursor;
+
+        public Itr() {
+            this.cursor = 0;
+        }
+
+        @Override
+        public boolean hasNext() { throw new UnsupportedOperationException(); }
+
+        @Override
+        public T next() {
+            return DIYarrayList.this.get(cursor++);
+        }
+
+        @Override
+        public boolean hasPrevious() { throw new UnsupportedOperationException(); }
+
+        @Override
+        public T previous() { throw new UnsupportedOperationException(); }
+
+        @Override
+        public int nextIndex() { throw new UnsupportedOperationException(); }
+
+        @Override
+        public int previousIndex() { throw new UnsupportedOperationException(); }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void set(T t) {
+            DIYarrayList.this.set(cursor-1, t);
+        }
+
+        @Override
+        public void add(T t) { throw new UnsupportedOperationException(); }
+    }
+}
